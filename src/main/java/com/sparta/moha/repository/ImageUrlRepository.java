a739070cde0dad11cf764480ae19257168b96c4f
hmhmchm <hyeonminchoi5@gmail.com>
2022-04-11 16:05:43 +0900

    삭제

--- src/main/java/com/sparta/moha/repository/ImageUrlRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.moha.repository;
-
-import com.sparta.moha.model.ImageUrl;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageUrlRepository extends JpaRepository<ImageUrl, Long> {
-}
--- src/test/java/com/sparta/moha/PostTest.java
+++
@@ -1,5 +0,0 @@
-package com.sparta.moha;
-
-public class PostTest {
-
-}
